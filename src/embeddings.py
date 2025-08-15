1ffca2f035959974868003cfa81b23b98981fd63
Ayush Ranjan <ranjanayush918@gmail.com>
2025-08-16 02:58:48 +0530

    Ri

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,6 +1,7 @@
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+# This is the corrected import path
+from langchain_community.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 
 
